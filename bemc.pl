d7687f0d9a512319392b2bd8f1f58b72d70cd6c7
mokhov <mokhov@yandex-team.ru>
2011-02-26 16:39:39 +0500

    Поиск папки, в которой есть blocks или block -- улучшаем код

--- bemc.pl
+++ bemc.pl
@@ -2,17 +2,27 @@
 
 # first let's find project root folder (it must have "blocks" or "block" folder in it)
 
+sub makepath {return join('/', @_)}
+
 use Cwd 'abs_path';
 my @path = split('/',abs_path($0));
 pop @path;
 
-while (!-d join('/', @path) . '/blocks' && !-d join('/', @path) . '/block' && $#path >= 0)
+my $blocks_path;
+
+while ($#path >= 0)
 {
-    print join('/', @path) . "/blocks\n";
+    my @tmp = grep(-d, map(makepath(@path) . "/" . $_, ('blocks', 'block')));
+    print '@tmp' . "\n";
+    print "@tmp\n";
+    print '@tmp --end' . "\n";
+
+    last if (grep(-d, map(makepath(@path) . "/" . $_, ('blocks', 'block'))));
+    #last if !-d ($blocks_path = makepath(@path) . "/blocks") || !-d ($blocks_path = makepath(@path) . "/block");
     pop @path;
 }
 
-print $#path;
+print $blocks_path;
 
 sub parseParams
 {
