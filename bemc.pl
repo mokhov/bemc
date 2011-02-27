5a96f2417933b7f85179b7ecf8b99764864450ea
mokhov <mokhov@yandex-team.ru>
2011-02-28 01:14:18 +0500

    Парсинг параметров: учим создавать структуру БЭМ

--- bemc.pl
+++ bemc.pl
@@ -9,23 +9,63 @@ my @path = split('/',abs_path($0));
 pop @path;
 
 my $blocks_path;
+my @tmp;
 
 while ($#path >= 0)
 {
-    my @tmp = grep(-d, map(makepath(@path) . "/" . $_, ('blocks', 'block')));
-    print '@tmp' . "\n";
-    print "@tmp\n";
-    print '@tmp --end' . "\n";
+    @tmp = grep(-d, map(makepath(@path) . "/" . $_, ('blocks', 'block')));
 
-    last if (grep(-d, map(makepath(@path) . "/" . $_, ('blocks', 'block'))));
-    #last if !-d ($blocks_path = makepath(@path) . "/blocks") || !-d ($blocks_path = makepath(@path) . "/block");
+    last if ($#tmp > -1);
     pop @path;
 }
 
-print $blocks_path;
+$blocks_path = $tmp[0] . '/';
 
-sub parseParams
-{
+print "path: $blocks_path\n";
+
+
+# now $blocks_path has path to blocks folder
+use Switch;
+
+my $b; my $e; my $m;
+my $current_folder;
+foreach $arg(@ARGV) {
+    if ($arg =~ /^b-\w+/) 
+    {
+        print "$arg is block\n";
+	  
+	$b = $arg;
+        $e = "";
+
+	$current_folder = $blocks_path . $arg;
+    }
+    
+    elsif ($arg =~ /^_{2}\w+/) 
+    {
+        print "$arg is element\n";
+
+        $e = $arg;
+	$e =~ s/__//;
+
+	$current_folder = $blocks_path . $b . '/' . $e;
+    }
+
+    elsif ($arg =~ /^_\w+_\w+/)
+    {
+        print "$arg is modifier\n";
+
+	$m = $arg;
+	my @tmp = $m =~ /(_[^_]+)/;
+
+	$current_folder = $blocks_path . $b . '/' . ($e ? $e . '/': '') . $tmp[0]; 
+    }
+
+    else 
+    {
+        print "$arg is filetype\n";
+    }
+
+    # if folder doen't exist - let's do ir
+    `mkdir $current_folder` if (!-d $current_folder);
 }
-
 
